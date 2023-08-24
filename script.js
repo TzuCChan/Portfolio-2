de1bf124a1d00239637165139d08e034d028bad6
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-08-24 17:15:27 -0400

    themeSwitch in js

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ if (theme == null) {
   setTheme('light')
 }
 
-let themeModes = document.getElementsByClassName("theme-mode");
+let themeSwitches = document.getElementsByClassName("theme-switch");
+
+for (let i = 0; themeSwitches.length > i; i++) {
+  themeSwitches[i].addEventListener("click", function () {
+    let mode = this.dataset.mode
+    setTheme(mode)
+  })
+}
